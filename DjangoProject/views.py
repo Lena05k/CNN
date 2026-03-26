d0b994fd7970e0a4496a31b6572b3d450b650460
Elena Krupoderova <lena05k@yandex.ru>
2026-03-26 12:07:57 -0700

    fix: return JSON from predictImage, fix ANTIALIAS->LANCZOS, fix typo Альбатрос, fix template name

--- DjangoProject/views.py
+++ DjangoProject/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from django.http import JsonResponse
 from django.conf import settings
 import onnxruntime
 import numpy as np
 from PIL import Image
 import os
 
-imageClassList = {'0': 'Пингвин', '1': 'Тупик', '2': 'Альбатрос'}  #Сюда указать классы
+imageClassList = {'0': 'Пингвин', '1': 'Тупик', '2': 'Альбатрос'}
 
 def scoreImagePage(request):
     return render(request, 'index.html')
 
 def predictImage(request):
-    fileObj = request.FILES['filePath']
-    fs = FileSystemStorage()
-    filePathName = fs.save('images/'+fileObj.name,fileObj)
-    filePathName = fs.url(filePathName)
-    modelName = request.POST.get('modelName')
-    scorePrediction = predictImageData(modelName, '.'+filePathName)
-    context = {'scorePrediction': scorePrediction}
-    return render(request, 'index.html', context)
+    try:
+        fileObj = request.FILES['filePath']
+        fs = FileSystemStorage()
+        filePathName = fs.save('images/' + fileObj.name, fileObj)
+        filePathName = fs.url(filePathName)
+        modelName = request.POST.get('modelName')
+        scorePrediction = predictImageData(modelName, '.' + filePathName)
+        return JsonResponse({'prediction': scorePrediction})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
 
 def predictImageData(modelName, filePath):
     img = Image.open(filePath).convert("RGB")
-    img = np.asarray(img.resize((32, 32), Image.ANTIALIAS))
+    img = np.asarray(img.resize((32, 32), Image.LANCZOS))
     model_path = os.path.join(settings.MEDIA_ROOT, 'cifar100_CNN_RESNET20.onnx')
     sess = onnxruntime.InferenceSession(model_path)
     outputOFModel = np.argmax(sess.run(None, {'input': np.asarray([img]).astype(np.float32)}))
